e8d410643bcfb767d8657b0bebbabf646d97afca
gvmartin07 <gvmartin@ualr.edu>
2025-09-08 14:55:33 +0000

    update

--- 02.Project.py
+++ 02.Project.py
@@ -5,18 +5,15 @@ from math import sin
 from math import cos
 # Earth radius = 6371 km
 r = float(input("Enter radius of sphere: "))
-y1 = float(input("Enter starting point latitude: "))
-#converting starting point lat to radians
-y1 = float(y1*pi/180)
-x1 = float(input("Enter starting point longitude: "))
-#converting starting point log to radians
-x1 = float(x1*pi/180)
-y2 = float(input("Enter ending point latitude: "))
-#converting ending point lat to radians
-y2 = float(y2*pi/180)
-x2 = float(input("Enter ending point longitude: "))
-#converting ending point log to radians
-x2 = float(x2*pi/180)
+lat1 = float(input("Enter starting point latitude: "))
+log1 = float(input("Enter starting point longitude: "))
+lat2 = float(input("Enter ending point latitude: "))
+log2 = float(input("Enter ending point longitude: "))
+#converting latitudes and longitudes to radians
+y1 = (lat1*pi/180)
+x1 = (log1*pi/180)
+y2 = (lat2*pi/180)
+x2 = (log2*pi/180)
 #finding great circle distance
 gcdistance = r * acos(sin(x1)) * sin(x2) + cos(x1) * cos(x2) * cos(y1-y2)
 print(round(gcdistance, 2))
