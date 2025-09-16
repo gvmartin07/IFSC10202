cd0779c93a110b186d252e87b754ff78334e8e3a
gvmartin07 <gvmartin@ualr.edu>
2025-09-16 21:49:44 +0000

    update

--- 05.project.py
+++ 05.project.py
@@ -4,20 +4,18 @@
 #special num 153 = 1^3 + 5^3 + 3^3
 startrange = int(input("Enter start of range: "))
 endrange = int(input("Enter end of range: "))
-endrange += 1
-for i in range(startrange, endrange):
+for i in range(startrange, endrange+1):
     #finding number of digits in value
     number = i
     digits = 0
     while number > 0:
         number = number//10
         digits += 1
-    #finding value of ones, tens, etc
-    while i != 0:
-        ones = i % 10
-        tens = i // 10
-        hundreds = i // 100
-        thousands = i // 1000
-        #finding if value is special number
-        if ones**digits + tens**digits + hundreds**digits + thousands**digits == i:
-            print(i)
+    number = i
+    sum = 0
+    while number > 0:
+        value = number%10
+        sum += value**digits
+    if sum == i:
+    #finding if value is special number
+        print(i)
