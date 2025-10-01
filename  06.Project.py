1e5892d1f3fdbed967ac330b89d6b365d00d5846
gvmartin07 <gvmartin@ualr.edu>
2025-10-01 14:31:32 +0000

    update

---  06.Project.py
+++  06.Project.py
@@ -1,11 +1,33 @@
 inputfile = open("06.Project Input File.txt", "r")
                   
-outputfile = open(06.Project Output File.txt", "w")
+outputfile = open("06.Project Output File.txt", "w")
 
-x = inputfile.read()
+mergefile = open("06.Project Merge File.txt")
 
-if x == "**Insert Merge File Here**":
-    mergefile = open("06.Project Merge File.txt")
+y = mergefile.readline()
+x = inputfile.readline()
 
+i = -1
+o = 0
+m = 0
+
+#copying input file to output file
+while x != "":
+    if x == ("**Insert Merge File Here**\n"):
+        #copying merge file into output file
+        while y != "":
+            outputfile.write(y)
+            y = mergefile.readline()
+            m += 1
+        x = '\n'
+    outputfile.write(x)
+    x = inputfile.readline()
+    i += 1
+# finding output record
+o = i+m
+print(i, "input files recorded")
+print(m, "merge files recorded")
+print(o, "output files recorded")
 inputfile.close()
 outputfile.close()
+mergefile.close()
