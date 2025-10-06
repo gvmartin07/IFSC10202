8071e6d0cbfa759f331fff9e3126d3ba5e9c4d56
gvmartin07 <gvmartin@ualr.edu>
2025-10-06 15:11:35 +0000

    update

---
+++ 07.Project.py
@@ -0,0 +1,5 @@
+#grab ddmmss from txt file then convert and write to output file, print amount of data processed
+# degree symbol is chr(176)
+angles = open("07.Project Angles Input.txt")
+def ParseDegreeString(ddmmss):
+    
